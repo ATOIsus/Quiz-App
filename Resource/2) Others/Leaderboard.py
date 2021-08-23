63ca5d89cd4bbd61c182f3039f0baa61c1dc4cad
ATOIsus <sabinmaharjan884@gmail.com>
2021-08-23 21:13:15 +0545

    Renamed

--- Resource/2) Others/Leaderboard.py
+++ Resource/2) Others/Leaderboard.py
@@ -1,13 +1,9 @@
-
-
 from tkinter import *
 import sqlite3
 from tkinter import messagebox
 
 root = Tk()
 root.title('Database')
-
-
 
 '''
 # Creating the table
@@ -29,10 +25,6 @@ conn.commit()
 conn.close()
 '''
 
-
-
-
-
 # Creating text boxes.
 
 name_ent = Entry(root, width=30)
@@ -43,8 +35,6 @@ uname_ent.grid(row=1, column=1)
 
 password_ent = Entry(root, width=30)
 password_ent.grid(row=2, column=1)
-
-
 
 # Creating textbox labels.
 
@@ -58,29 +48,24 @@ password_lbl = Label(root, text="Password")
 password_lbl.grid(row=2, column=0)
 
 
-
 # Submit region
 
 def submit_fun():
-
-    conn = sqlite3.connect('playerData.db')  
+    conn = sqlite3.connect('playerData.db')
     c = conn.cursor()
-
 
     c.execute("INSERT INTO information VALUES (:Name, :Username, :Password, :Score)",
               {
-                'Name': name_ent.get(),
-                'Username': uname_ent.get(),
-                'Password': password_ent.get(),
-                'Score' : 0
+                  'Name': name_ent.get(),
+                  'Username': uname_ent.get(),
+                  'Password': password_ent.get(),
+                  'Score': 0
               })
 
-    messagebox.showinfo("Completed","Record inserted")
-    
+    messagebox.showinfo("Completed", "Record inserted")
+
     conn.commit()
     conn.close()
-
-
 
 
 # Creating submit button.
@@ -92,73 +77,58 @@ submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 # Submit region
 
 
-
-
 # Show region
 
 def query_fun():
-
-    conn = sqlite3.connect('playerData.db')  
+    conn = sqlite3.connect('playerData.db')
     c = conn.cursor()
 
     c.execute("SELECT *, oid FROM information")
 
     records = c.fetchall()
-    
+
     record_str = ''
-    query_lbl = Label(root,text = record_str)
+    query_lbl = Label(root, text=record_str)
     query_lbl.grid(row=8, column=0, columnspan=2)
 
     for record in records:
-        record_str +=  str(record[0]) + ' ' + str(record[1]) + ' ' + str(record[2])  + ' ' + str(record[3]) + '\n'
+        record_str += str(record[0]) + ' ' + str(record[1]) + ' ' + str(record[2]) + ' ' + str(record[3]) + '\n'
 
-    query_lbl.configure(text = record_str)
+    query_lbl.configure(text=record_str)
 
     conn.commit()
     conn.close()
 
 
-query_btn = Button(root, text = "Show Records", command = query_fun)
-query_btn.grid(row = 7, column = 0, columnspan = 2, padx = 10, pady = 10, ipadx = 100)
+query_btn = Button(root, text="Show Records", command=query_fun)
+query_btn.grid(row=7, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
 
 # Show region
 
 
-
-
-
 # Delete region
 
-delete_lbl = Label(root, text = "Delete ID")
+delete_lbl = Label(root, text="Delete ID")
 delete_lbl.grid(row=9, column=0, pady=5)
 
-delete_ent = Entry(root, width = 30)
-delete_ent.grid(row = 9, column = 1, pady = 5)
+delete_ent = Entry(root, width=30)
+delete_ent.grid(row=9, column=1, pady=5)
 
 
 def delete_fun():
-    
     conn = sqlite3.connect('playerData.db')
     c = conn.cursor()
 
-    c.execute("DELETE FROM information WHERE oid = " + delete_ent.get())
+    c.execute("DELETE from information WHERE oid = " + delete_ent.get())
 
     conn.commit()
     conn.close()
 
 
-delete_btn = Button(root, text = "Delete", command = delete_fun)
-delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx = 10, ipadx = 120)
-   
-
+delete_btn = Button(root, text="Delete", command=delete_fun)
+delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=120)
 
 # Delete region
 
 
-
-
-
 root.mainloop()
-
-
-
