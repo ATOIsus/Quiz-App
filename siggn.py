0532738175082cdb7f5e511a72b418113167dd76
bis-hub <sthbishal8@gmail.com>
2021-08-23 13:18:24 +0545

    code updated

--- siggn.py
+++ siggn.py
@@ -8,7 +8,7 @@ class sign:
         self.root.title("SIGN IN")
         self.root.geometry("700x500")
         self.root.iconbitmap("user.ico")
-        self.root.configure(bg="cyan")
+        self.root.configure(bg="#00cccc")
 
         frame=Frame(self.root,bg="white")
         frame.place(x=150,y=150,height=300,width=400)
@@ -17,21 +17,21 @@ class sign:
         lbl.place(x=60,y=30)
 
         lbl1=Label(frame,text="Username",font=("arial",15,"bold"),fg="green",bg="white")
-        lbl1.place(x=60,y=100)
-        self.entry=Entry(frame,font=("arial,15"),bg="light green")
-        self.entry.place(x=60,y=128,width="300",height="30")
+        lbl1.place(x=60,y=90)
+        self.entry1=Entry(frame,font=("arial,15"),bg="light green")
+        self.entry1.place(x=60,y=120,width="300",height="30")
 
-        lbl1 = Label(frame, text="E-mail", font=("arial", 15, "bold"), fg="green", bg="white")
-        lbl1.place(x=60, y=170)
-        self.entry1 = Entry(frame, font=("arial,15"), bg="light green")
-        self.entry1.place(x=60, y=200, width="300", height="30")
+        lbl2 = Label(frame, text="Password", font=("arial", 15, "bold"), fg="green", bg="white")
+        lbl2.place(x=60, y=160)
+        self.entry2 = Entry(frame, font=("arial,15"), bg="light green")
+        self.entry2.place(x=60, y=190, width="300", height="30")
 
-        btn=Button(frame,text="sign in",bd=5,bg="green",fg="white",font=("arial",15),command=self.signn)
+        btn=Button(frame,text="sign in",bd=4,bg="green",fg="white",font=("arial",15),command=self.signn)
         btn.place(x=160,y=240)
 
     def signn(self):
-        if self.entry.get()=="" or self.entry1.get()=="":
-            messagebox.showerror("Error","All fields are required",parent=self.root)
+        if self.entry1.get() == "" or self.entry2.get() == "":
+            messagebox.showerror("Error", "All fields are required", parent=self.root)
         else:
             messagebox.showinfo("Congrats","You are signned in",parent=self.root)
 
