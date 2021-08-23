d73f97f58fd7dbbcd92d98d50ec06d9c78398c61
ATOIsus <sabinmaharjan884@gmail.com>
2021-08-23 15:41:50 +0545

    Errors in question pickling taken care of

--- Resource/3) Others/Pickling.py
+++ Resource/3) Others/Pickling.py
@@ -2,19 +2,29 @@
 
 import pickle
 
-ques = (['Is Python case sensitive when dealing with identifiers? ', ' Yes ', ' No ', ' Machine dependent ', ' None of the mentioned'],
-        ['What is the maximum possible length of an identifier? ', ' none of the mentioned ', ' 63 characters ', ' 79 characters ', ' 31 characters '],
-        ['Which of the following is an invalid variable? ', ' 1st_string ', ' my_string_1 ', ' foo ', ' _'],
-        ['Why are local variable names beginning with an underscore discouraged? ', ' They are used to indicate a private variables of a class ', ' They confuse the interpreter ', ' They are used to indicate global variables ', ' They slow down execution'],
-        ['Which of the following is invalid? ', ' None of the mentioned ', ' __a = 1 ', ' __str__ = 1 ', ' _a = 1'],
-        ['Which of the following is not a keyword? ', ' eval ', ' assert ', ' nonlocal ', ' pass'],
-        ['All keywords in Python are in _________ ', ' None of the mentioned ', ' UPPER CASE ', ' Capitalized ', ' Lower case '],
-        ['Which of the following is true for variable names in Python? ', ' Unlimited length ', ' All private members must have leading and trailing underscores ', ' Underscore and ampersand are the only two special characters allowed ', ' None of the mentioned'],
-        ['Which of the following is an invalid statement? ', ' a b c = 1000 2000 3000 ', ' abc = 1', '000', '000 ', ' a', 'b', 'c = 1000', ' 2000', ' 3000 ', ' a_b_c = 1', '000', '000'], ['Which of the following cannot be a variable? ', ' in ', ' __init__ ', ' it ', ' on'], ['What is the output of print 0.1 + 0.2 == 0.3? ', ' False ', ' True ', ' Machine dependent ', ' Error'],
-        ['Which of the following is not a complex number? ', ' k = 2 + 3l ', ' k = complex(2', ' 3) ', ' k = 2 + 3j ', ' k = 2 + 3J'], ['What is the type of inf? ', ' Float ', ' Integer ', ' Boolean ', ' Complex'], ['What does ~4 evaluate to? ', ' -5 ', ' -4 ', ' -3 ', ' +3'], ['What does ~~~~~~5 evaluate to? ', ' +5 ', ' -11 ', ' +11 ', ' -5'], ['Which of the following is incorrect? ', ' x = 03964 ', ' x = 0x4f5 ', ' x = 19023 ', ' x = 0b101'], ['What is the result of cmp(3', ' 1)? ', ' 1 ', ' 0 ', ' True ', ' False'], ['What is the result of round(0.5) – round(-0.5)? ', ' 2.0 ', ' 1.0 ', ' 0.0 ', ' None of the mentioned'], ['Which of these about a set is not true? ', ' Immutable data type ', ' Allows duplicate values ', ' Data type with unordered values ', ' Mutable data type'],
-        ['Which of the following statements is used to create an empty set? ', ' set() ', ' { } ', ' [ ] ', ' ( )'])
+ques = (['Which of the following is the set of positive integers? ', ' Infinite ', ' Subset ', ' Finite ', ' Empty'],
+        ["Which of the following is union of {'1', '2', '5'} and {'1', '2', '6'} ? ", " {'1' , '2', '5', '6'} ", " {'1', '2', '6', '1'} ", " {'1', '2', '1', '2'} ", " {'1', '5', '6', '3'}"],
+        ['Which of the following is complement of the set A? ', ' U – A  ', ' A – B ', ' A – U ', ' B – A'],
+	['The relation between sets A', ' B', ' C as shown by venn diagram is ', ' C is subset of B and B is subset of A ', ' A is subset of B and B is subset of C ', ' C is not a subset of A and A is subset of B ', ' None of These'],
+        ['Which of the following statement is false? ', ' (A U B)’ = A’ U B’  ', ' A X A = A ', ' A U A = A ', ' A – (B U C) = (A – B) U (A –C)'],
+	['If a set contains 3 elements then the number of subsets are? ', ' 8 ', ' 6 ', ' 3 ', ' 12'],
+        ['How many bytes are required to encode 2000 bits of data? ', ' 2 ', ' 8 ', ' 5 ', ' 32'],
+	['Floor (2.4) + Ceil (2.9) is equal to ', ' 5 ', ' 1 ', ' 6.3 ', ' 7'],
+        ['Which of the following is a collection of graph? ', ' Vertices and columns ', ' Row and column  ', ' Equation ', ' None of above'],
+	['Which of the following is a error correcting code? ', ' Hamming code ', ' Error deducting code ', ' Gray code ', ' None of the above'],
+	['The set of positive integers under the operation of ordinary multiplication is ', ' An Abelian group ', ' A group ', ' Not a group ', ' Not a monoid'],
+        ['The number of eight-bit strings beginning with either 111 or 101 is - ', ' 64 ', ' 128 ', ' 256 ', ' 312'],
+        ['Let A and B be two arbitrary events,  then ', ' P(AUB) not equal to P(A)+P(B) ', ' P(A X B) = P(A) X P (B) ', ' P(AUB) = P(A)+P(B) ', ' P(A/B) = P(A - B)+P(B)'],
+	['The sum of square of the first n natural numbers is given by ', ' n(n+1)(2n+1)/6 ', ' n(n-1)/2(2n+1) ', ' n2(n+1)(2n+1)/6 ', ' None of these'],
+	["The sequence '1 ', ' 1', ' 1', ' 1', ' 1' …. is ", ' Not absolutely summable ', ' Absolutely summable ', ' Can’t say ', ' None of These'],
+        ["Which of the following is cardinality of the set A = {' 1', ' 2', ' 3', ' 4', ' 6}? ", ' 5 ', ' 9 ', ' 6 ', ' 3'],
+        ['A matrix having many rows and one column is known as - ', ' Column matrix  ', ' Row matrix ', ' Diagonal matrix ', ' None of the above'],
+        ['Which of the following sentence is a mistake? ', ' You ', ' The earth is round'],
+	['Let A order (axb) and Border(cxd) be two matrices,  then if AB exists,  the order of AB is? ', ' axd ', ' bxc ', ' axb ', ' cxd'],
+	['If determinant of a matrix A is Zero then ', ' A is a Singular matrix ', ' A is a non Singular matrix ', ' Can’t say ', ' None of These'])
 
-file = open('Programming.txt', 'ab')
+
+file = open('Math.txt', 'ab')
 
 
 pickle.dump(ques, file)
