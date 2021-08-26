5bed50bc3f8f92dd332876ee7d310df7ea1a55a8
ATOIsus <sabinmaharjan884@gmail.com>
2021-08-26 22:04:37 +0545

    Hopefully the last edit for this file.

--- Resource/2) Others/Convert.py
+++ Resource/2) Others/Convert.py
@@ -1,4 +1,4 @@
-# Converts 20 questions inserted, into list of Question and choices within a parent list.
+# Converts 20 questions inserted, into 20 list of Question and choices within a single parent list.
 
 from tkinter import messagebox
 
@@ -11,9 +11,9 @@ try:
 
         inp = input(f"Enter {i} : ")  # Question  A. Answer(choice)  B. Choice_1 C. Choice_2 D. Choice_3
 
-        # Make sure to add something unique and least used sequence
+        # Make sure to add something unique and possibly least used sequence of random characters.
         # There were errors when ',' was used;
-        # commas were already in the question and it created more than 5 elements.
+        # When commas ',' were already in the question and it created more than 5 elements.
 
         b = inp.replace('A.', 'zzzzz')  # Question  zzzzz Answer(choice)  B. Choice_1 C. Choice_2 D. Choice_3
         c = b.replace('B.', 'zzzzz')  # Question  zzzzz Answer(choice)  zzzzz Choice_1 C. Choice_2 D. Choice_3
@@ -23,12 +23,10 @@ try:
         # Its okay if there are only 2 or 3 choices.
         # This is taken care of in the question.py program.
 
-        list_individual = e.split(
-            "zzzzz")  # ['Question  ', ' Anser(choice)  ', ' Choice_1 ', ' Choice_2 ', ' Choice_3']
+        list_individual = e.split("zzzzz")  # ['Question  ', ' Anser(choice)  ', ' Choice_1 ', ' Choice_2 ', ' Choice_3']
         # 5 elements: Question, Answer, Option_1, Option_2, Option_3
 
-        total_list.append(
-            list_individual)  # [['Question  ', ' Anser(choice)  ', ' Choice_1 ', ' Choice_2 ', ' Choice_3']]
+        total_list.append(list_individual)  # [['Question  ', ' Anser(choice)  ', ' Choice_1 ', ' Choice_2 ', ' Choice_3']]
 
     print(total_list)  # Prints a single list of 20 MCQs lists.
 
