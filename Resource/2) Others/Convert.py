6ed704e768fbe0dc22f24d3da754f4273f71dca1
ATOIsus <sabinmaharjan884@gmail.com>
2021-08-26 21:13:38 +0545

    Code for Convert program explained

--- Resource/2) Others/Convert.py
+++ Resource/2) Others/Convert.py
@@ -1,23 +1,18 @@
+# Converts 20 questions inserted, into list of Question and choices within a parent list.
 
 
-
-
-all = []
+total_list = []
 
 for i in range(1, 21):
 
-    inp = input(f"Enter {i} : ")
+    inp = input(f"Enter {i} : ")   # Question  A. Answer(choice)  B. Choice_1 C. Choice_2 D. Choice_3
 
-    b = inp.replace('A.', ',')
-    c = b.replace('B.', ',')
-    d = c.replace('C.', ',')
-    e = d.replace('D.', ',')
+    b = inp.replace('A.', ',')     # Question  , Answer(choice)  B. Choice_1 C. Choice_2 D. Choice_3
+    c = b.replace('B.', ',')       # Question  , Answer(choice)  , Choice_1 C. Choice_2 D. Choice_3
+    d = c.replace('C.', ',')       # Question  , Answer(choice)  , Choice_1 , Choice_2 D. Choice_3
+    e = d.replace('D.', ',')       # Question  , Answer(choice)  , Choice_1 , Choice_2 , Choice_3
 
-    list_1 = e.split(",")
-    all.append(list_1)
+    list_individual = e.split(",")      # ['Question  ', ' Anser(choice)  ', ' Choice_1 ', ' Choice_2 ', ' Choice_3']
+    total_list.append(list_individual)  # [['Question  ', ' Anser(choice)  ', ' Choice_1 ', ' Choice_2 ', ' Choice_3']]
 
-
-
-print(all)
-
-
+print(total_list)
