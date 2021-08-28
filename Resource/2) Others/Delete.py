5fdc33ac71b04564cb2fd92d9c756958c0a3caab
ATOIsus <sabinmaharjan884@gmail.com>
2021-08-28 17:09:31 +0545

    Separate module for Delete and Questoin Function created, and code explained.

---
+++ Resource/2) Others/Delete.py
@@ -0,0 +1,59 @@
+from tkinter import *              # To use basic functions and methods of tkinter.
+from tkinter import messagebox     # To show popup messagebox.
+import sqlite3                     # To connect to the database.
+
+
+root = Tk()
+root.title('Database')
+root.geometry('300x100')
+root.resizable(False, False)
+
+
+
+try:
+
+    # region Delete Function.
+
+    delete_lbl = Label(root, text="ID")
+    delete_lbl.grid(row=2, column=0, pady=5)
+
+    delete_ent = Entry(root, width=30)
+    delete_ent.grid(row=2, column=1, pady=5)
+
+
+    def delete_fun():
+
+        """ Deletes the row with the given ID,
+        but while implementing it will delete the row of the current user only (Get ROWID in sign in function).  """
+
+        try:
+            conn3 = sqlite3.connect('Player Database.db')
+            c3 = conn3.cursor()
+
+            c3.execute(f"DELETE from information WHERE ROWID = {delete_ent.get()}")
+
+            conn3.commit()
+            conn3.close()
+
+            delete_ent.delete(0, END)
+
+            messagebox.showinfo("Success", "Account deleted!")
+
+        except BaseException as er:
+            messagebox.showerror("Error in Delete Function!", str(type(er))[6:-1] + " : " + str(er))
+
+
+
+    delete_btn = Button(root, text="Delete", command=delete_fun)
+    delete_btn.grid(row=4, column=0, columnspan=2, pady=10, padx=10, ipadx=120)
+
+    # endregion
+
+
+except BaseException as er1:
+    messagebox.showerror("Error", str(type(er1))[6:-1] + " : " + str(er1))
+    root.destroy()
+
+
+else:
+    root.mainloop()
---
+++ Resource/2) Others/Question.py
@@ -0,0 +1,97 @@
+import random  # To shuffle choices.
+import pickle  # To unpickle the MCQ.
+from tkinter import messagebox  # To display error message.
+
+
+question = []  # List of all questions.
+anser = []     # List of all answers.
+option1 = []   # List of all option.
+option2 = []   # List of all option.
+option3 = []   # List of all option.
+score = 0      # The number of correct answers.
+ques = []      # List of all MCQs.
+question_no = 1  # The indexing for the question in the final program.
+
+try:
+    ''' To unpickle the MCQs '''
+
+    file_open = input("Enter the file to open: ")
+    file = open(f'Resource/1) Question/{file_open}.txt', 'rb')
+
+    ques = list(pickle.load(file))  # Tuple converted into list.
+
+except BaseException as er1:
+    messagebox.showerror("Error", str(type(er1))[6:-1] + " : " + str(er1))
+
+
+try:
+    ''' Question, answer and options are separated into different lists. '''
+
+    for i in ques:
+
+        if len(i) == 3:  # If there are only 2 options.
+            option2.append(' Both')
+            option3.append(' Neither')
+
+        elif len(i) == 4:  # If there are 3 options.
+            option3.append(' None')
+
+        for j in range(len(i)) :
+            if j == 0:
+                question.append(str(i[0]))
+            elif j == 1:
+                anser.append(str(i[1]))
+            elif j == 2:
+                option1.append(str(i[2]))
+            elif j == 3:
+                option2.append(str(i[3]))
+            elif j == 4:
+                option3.append(str(i[4]))
+
+except BaseException as er2:
+    messagebox.showerror("Error", str(type(er2))[6:-1] + " : " + str(er2))
+
+
+try:
+    ''' Question and choices are displayed; the answer is checked and scored. '''
+
+    def chk_ans(ans, k):
+
+        global score
+        try:
+            if choice[ans - 1] == anser[k]:  # Check if the answer is right.
+                score += 1
+            else:
+                print()
+                print(f"Wrong! The right answer was:  {anser[k]}")
+                print()
+        except BaseException as er3:
+            messagebox.showerror("Error", str(type(er3))[6:-1] + " : " + str(er3))
+
+
+    for l in range(len(ques)):
+
+
+        choice = [anser[l], option1[l], option2[l], option3[l]]
+        random.shuffle(choice)
+
+        print()
+        print(f'{question_no})  {question[l]}')
+        print()
+
+        print('1) ' + choice[0])
+        print('2) ' + choice[1])
+        print('3) ' + choice[2])
+        print('4) ' + choice[3])
+        print()
+
+        answer = int(input('Enter the answer number: '))
+
+        chk_ans(answer, l)  # Function is called.
+
+        question_no += 1
+
+    print(f'Your score is {score} out of {len(ques)}')  # Final score.
+
+except BaseException as er4:
+    messagebox.showerror("Error", str(type(er4))[6:-1] + " : " + str(er4))
