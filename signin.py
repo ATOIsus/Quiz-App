079c2d87b6cb6627dd94ce3d136bef30d56f9b34
bis-hub <sthbishal8@gmail.com>
2021-09-02 13:25:21 +0545

    updated

--- signin.py
+++ signin.py
@@ -13,16 +13,16 @@ class signin:
         frame=Frame(self.root,bg="white")
         frame.place(x=150,y=150,height=300,width=400)
 
-        lbl=Label(frame,text="Sign In",font=("Times",30,"bold"),fg="green",bg="white")
-        lbl.place(x=60,y=30)
+        s_in=Label(frame,text="Sign In",font=("Times",30,"bold"),fg="green",bg="white")
+        s_in.place(x=60,y=30)
 
-        lbl1=Label(frame,text="Username",font=("arial",15,"bold"),fg="green",bg="white")
-        lbl1.place(x=60,y=90)
+        user_n=Label(frame,text="Username",font=("arial",15,"bold"),fg="green",bg="white")
+        user_n.place(x=60,y=90)
         self.entry1=Entry(frame,font=("arial,15"),bg="light green")
         self.entry1.place(x=60,y=120,width="300",height="30")
 
-        lbl2 = Label(frame, text="Password", font=("arial", 15, "bold"), fg="green", bg="white")
-        lbl2.place(x=60, y=160)
+        p_word = Label(frame, text="Password", font=("arial", 15, "bold"), fg="green", bg="white")
+        p_word.place(x=60, y=160)
         self.entry2 = Entry(frame, font=("arial,15"), bg="light green",show="*")
         self.entry2.place(x=60, y=190, width="300", height="30")
 
