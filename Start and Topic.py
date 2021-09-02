108d9463614c5ae8f869b01ed02c1951aaf8e6f1
ATOIsus <sabinmaharjan884@gmail.com>
2021-09-02 21:36:24 +0545

    Start and Topic Display combined.

--- Start and Topic.py
+++ Start and Topic.py
@@ -4,6 +4,8 @@ from tkinter import *
 
 def Click():
 
+    root.destroy()
+
     root2 = Tk()
     root2.title("Quiz")
     root2.geometry("700x600")
@@ -62,4 +64,41 @@ def Click():
     root2.mainloop()
 
 
-Click()
+
+
+# region 1) Staring Page.
+
+
+
+root = Tk()
+root.title("Quiz")
+root.geometry("700x600")
+root.config(background="#66b3ff")
+root.resizable(False, False)
+
+img1 = PhotoImage(file="Resource/2) Others/Images/quiz.png")
+labelImage = Label(root, image=img1, bg="#66b3ff")
+labelImage.pack(pady=(100, 0))
+
+labelText = Label(root, text="Quiz", font=("Corbel", 23, "bold"), bg="#66b3ff")
+labelText.pack(pady=(5, 50))
+
+img2 = PhotoImage(file="Resource/2) Others/Images/start-button.png")
+startButton = Button(root, image=img2, bg="#66b3ff", relief="flat", border=0, command=Click)
+startButton.pack(pady=(5, 50))
+
+labelRule = Label(root,
+                  text="""
+                  How this Quiz works:
+                  There are 8 topics to choose from.
+                  Each topic has has a total of 20 questions.
+                  Once you click on an option, that will be the final answer.
+                  So think before you answer.
+                  """,
+                  width=100, font=("Cambria", 13), bg="#ff9933", fg="#000000")
+
+labelRule.pack()
+
+root.mainloop()
+
+# endregion
