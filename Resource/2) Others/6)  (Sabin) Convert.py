81aecea3a891d9e5407a1184a2b5467083a38d87
ATOIsus <sabinmaharjan884@gmail.com>
2021-09-07 17:54:25 +0545

    Additional Individual Files Added.

---
+++ Resource/2) Others/6)  (Sabin) Convert.py
@@ -0,0 +1,34 @@
+# Converts 20 questions inserted, into 20 list of Question and choices within a single parent list.
+
+from tkinter import messagebox
+
+try:
+
+    total_list = []
+
+    for i in range(1, 21):
+        # The comments below shows what happens in one iteration.
+
+        i = input(f"Entry {i} : ")  # Question  A. Answer(choice)  B. Choice_1 C. Choice_2 D. Choice_3
+
+        # Make sure to add something unique and possibly least used sequence of random characters.
+        # There were errors when ',' was used;
+        # When commas ',' were already in the question and it created more than 5 elements.
+
+        b = i.replace('A.', 'zzzzz')  # Question  zzzzz Answer(choice)  B. Choice_1 C. Choice_2 D. Choice_3
+        c = b.replace('B.', 'zzzzz')  # Question  zzzzz Answer(choice)  zzzzz Choice_1 C. Choice_2 D. Choice_3
+        d = c.replace('C.', 'zzzzz')  # Question  zzzzz Answer(choice)  zzzzz Choice_1 zzzzz Choice_2 D. Choice_3
+        e = d.replace('D.', 'zzzzz')  # Question  zzzzz Answer(choice)  zzzzz Choice_1 zzzzz Choice_2 zzzzz Choice_3
+
+        # Its okay if there are only 2 or 3 choices.
+        # This is taken care of in the question.py program.
+
+        list_individual = e.split("zzzzz")  # ['Question  ', ' Anser(choice)  ', ' Choice_1 ', ' Choice_2 ', ' Choice_3']
+        # 5 elements: Question, Answer, Option_1, Option_2, Option_3
+
+        total_list.append(list_individual)  # [['Question  ', ' Anser(choice)  ', ' Choice_1 ', ' Choice_2 ', ' Choice_3']]
+
+    print(total_list)  # Prints a single list of 20 MCQs lists.
+
+except BaseException as er:
+    messagebox.showerror("Error", str(type(er))[6:-1] + " : " + str(er))
---
+++ Resource/2) Others/9)  (Sabin) Update.py
@@ -0,0 +1,120 @@
+from tkinter import *             # To use basic functions and methods of tkinter.
+from tkinter import messagebox    # To show popup messagebox.
+import sqlite3                    # To connect to the database.
+
+
+root = Tk()
+root.title('Update')
+root.geometry('220x100')
+root.resizable(False, False)
+
+
+# Global variables to be used in multiple functions.
+
+global username_var
+global password_var
+global quiz_topic
+global PlayerID
+global db_score
+
+
+
+try:  # Exception Handling.
+
+    # region Sign In.
+
+    def signin_fun():
+
+        """ Checks if the provided username & password is same to that stored in the database. """
+
+        global username_var
+        global password_var
+        global quiz_topic
+        global PlayerID
+        global db_score
+
+        try:
+
+            quiz_topic   = input("Enter the subject: ")
+            username_var = input("Enter Username: ")
+            password_var = input("Enter Password: ")
+
+            conn = sqlite3.connect('Database/Player Database.db')
+            cur = conn.cursor()
+
+            cur.execute(f"SELECT ROWID, Username, Password, {quiz_topic} FROM information")
+            records = cur.fetchall()
+
+
+            for record in records:
+
+                if username_var == str(record[1]) and password_var == str(record[2]):
+                    PlayerID = record[0]    # ROWID
+                    db_score = record[3]    # quiz_topic
+                    print(f'PlayerID {PlayerID}')
+                    print(f'Db_score {db_score}')
+                    break
+                else:
+                    pass
+            else:
+                messagebox.showerror('Error', 'Wrong Username or Password')
+
+            conn.commit()
+            conn.close()
+
+        except BaseException as er:
+            messagebox.showerror("Error while signing in", str(type(er))[6:-1] + " : " + str(er))
+
+
+    signin_btn = Button(root, text="Sign In", command=signin_fun)
+    signin_btn.grid(pady=10, padx=10, ipadx=60)
+
+
+    # endregion
+
+
+    # region Update Function.
+
+
+
+    def update_fun():
+
+        """ Updates the score if it is greater than that stored in the database of a selected topic. """
+
+        global db_score
+
+        try:
+            
+            score = int(input("Enter the score: "))
+    
+            if score > db_score :
+    
+                conn1 = sqlite3.connect('Database/Player Database.db')
+                cur1 = conn1.cursor()
+    
+                cur1.execute(f"UPDATE information SET {quiz_topic} = ? where ROWID = ?", (score, PlayerID))
+            
+                conn1.commit()
+                conn1.close()
+    
+            else:
+                pass
+
+        except BaseException as er1:
+            messagebox.showerror("Error in update function", str(type(er1))[6:-1] + " : " + str(er1))
+
+
+    update_btn = Button(root, text="Update", command=update_fun)
+    update_btn.grid(pady=10, padx=10, ipadx=60)
+
+
+    # endregion
+
+
+except BaseException as er2:
+    messagebox.showerror("Error within whole try block", str(type(er2))[6:-1] + " : " + str(er2))
+    root.destroy()
+
+
+else:
+    root.mainloop()
